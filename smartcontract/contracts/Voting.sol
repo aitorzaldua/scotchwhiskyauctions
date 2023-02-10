9ff9c7e027ad478dba427e9e809c263c7bb7e05d
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-02-10 15:10:05 +0100

    DAO voting system: newBallot(), vote()

--- smartcontract/contracts/Voting.sol
+++ smartcontract/contracts/Voting.sol
@@ -1,4 +1,4 @@
-//Only Members can open a votation 
+//Only Members can open a votation
 //Only Member can vote
 //From 1 to 3 tokens: 1 vote
 //From 3 to 10 tokens: 2 votes
@@ -9,13 +9,51 @@
 //Mimimal aproval 10% of the tokens available
 //Aproval: at end date -> 70% of emited votes
 
+//End date is always one week after the starting date.
+
+
 // SPDX-License-Identifier: MIT
 // @audit-issue - Don´t use ^ in mainnet.
 // Last update: 20230210
 pragma solidity ^0.8.4;
 
+import "hardhat/console.sol";
+
 import "./DaoMembers.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Voting is DaoMembers {
+
+
+    enum Status {
+        open,
+        closed
+    }
+
+    enum Outcome {
+        passed,
+        rejected,
+        enacted,
+        pending
+    }
+
+    struct Ballot {
+        string question;
+        address createdBy;
+        Counters.Counter votes;
+        uint tokenBalance;
+        Status status;
+        Outcome outcome;
+        string startingDate;
+        string endDate;
+    }
+
+    mapping(uint => Ballot) ballots;
+
+    function newBallot (string _question, ) public onlyRole(MEMBER_ROLE) {
+
+    }
+
+
 
 }
