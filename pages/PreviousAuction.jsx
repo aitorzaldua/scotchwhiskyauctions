f4f14571b5e0166760f629eaa0b8c097a98a6c37
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-02-17 16:12:12 +0100

    adding blockchain data

--- pages/PreviousAuction.jsx
+++ pages/PreviousAuction.jsx
@@ -6,14 +6,15 @@ import contractAbi from "./utils/contractAbi.json";
 
 const contractAddress = "0xa341b27f38cC658507593f2ECAbA4A15b10234f5";
 
+const whichOne = 1;
 
-const loadData = async () => {
+const loadData = async (whichOne = 1) => {
 
   try {
     if (ethereum) {
       const provider = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/lVH3kEFWP68nfURINIQvYAaW1JxDHGEL");
       const contract = new ethers.Contract(contractAddress,contractAbi,provider);
-      const dataObtained = await contract.getVoting(Membervoting);
+      const dataObtained = await contract.getVoting(whichOne);
       alert(dataObtained);
     } else {
       console.log("we don´t have Metamask object");
@@ -34,11 +35,12 @@ export default function PreviousAuction() {
         <NavBar />
       </div>
       <div
-        onClick={loadData(Membervoting)}
+        onClick={loadData}
         className="text-white bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]"
       >
         <button>click!</button>
       </div>
+   
     </div>
   );
 }
